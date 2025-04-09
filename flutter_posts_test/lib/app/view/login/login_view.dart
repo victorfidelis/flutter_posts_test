9cf61483526b5055cb1c44574a45df9c30a6689a
victorfidelis <fideliscorrea.victor@gmail.com>
2025-04-09 04:02:22 -0300

    login structure

--- flutter_posts_test/lib/app/view/login/login_view.dart
+++ flutter_posts_test/lib/app/view/login/login_view.dart
@@ -8,8 +8,51 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2.8,
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 700,
+              padding: const EdgeInsets.symmetric(horizontal: 38),
+              child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          label: Text('Email'),
+                        ),
+                        controller: emailController,
+                      ),
+                      const SizedBox(height: 18),
+                      TextField(
+                        decoration: InputDecoration(
+                          label: Text('Senha'),
+                        ),
+                        obscureText: true,
+                        controller: passwordController,
+                      ),
+                      const SizedBox(height: 32),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Logar'),
+                      ),
+                    ],
+                  )
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
