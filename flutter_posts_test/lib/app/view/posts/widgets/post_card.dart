b730b7a1e665d4b2ff7d8fe9fe579951121b8c26
victorfidelis <fideliscorrea.victor@gmail.com>
2025-04-11 01:25:47 -0300

    created resize of post card

--- flutter_posts_test/lib/app/view/posts/widgets/post_card.dart
+++ flutter_posts_test/lib/app/view/posts/widgets/post_card.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_posts_test/app/model/post.dart';
 
-class PostCard extends StatelessWidget {
+class PostCard extends StatefulWidget {
   const PostCard({super.key, required this.post});
 
   final Post post;
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  bool isExpanded = false;
+  bool get isExpanable => widget.post.body.length > 100;
+  String get text =>
+      isExpanded || !isExpanable ? widget.post.body : '${widget.post.body.substring(0, 100)}...';
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +38,51 @@ class PostCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            post.title,
+            widget.post.title,
             style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 8),
-          Text(post.body),
+          buildBody(),
         ],
       ),
     );
+  }
+
+  Widget buildBody() {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(text: text, style: TextStyle(color: Colors.black)),
+          buildClickableText(),
+        ],
+      ),
+    );
+  }
+
+  InlineSpan buildClickableText() {
+    if (isExpanable && !isExpanded) {
+      return buildSpanClickable('   Ver mais');
+    } else if (isExpanable && isExpanded) {
+      return buildSpanClickable('   Ver menos');
+    } else {
+      return TextSpan();
+    }
+  }
+
+  TextSpan buildSpanClickable(String text) {
+    final tapGesture = TapGestureRecognizer();
+    tapGesture.onTap = resize;
+
+    return TextSpan(
+      text: text,
+      recognizer: tapGesture,
+      style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+    );
+  }
+
+  void resize() {
+    setState(() {
+      isExpanded = !isExpanded;
+    });
   }
 }
