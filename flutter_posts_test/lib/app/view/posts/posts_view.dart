c89b1a31c017f2e2967acc34d46f06a7d3b4e1f3
victorfidelis <fideliscorrea.victor@gmail.com>
2025-04-12 02:05:15 -0300

    created header of home

--- flutter_posts_test/lib/app/view/posts/posts_view.dart
+++ flutter_posts_test/lib/app/view/posts/posts_view.dart
@@ -9,6 +9,7 @@ import 'package:flutter_posts_test/app/model/post.dart';
 import 'package:flutter_posts_test/app/repository/post/post_repository.dart';
 import 'package:flutter_posts_test/app/shared/widgets/custom_loading.dart';
 import 'package:flutter_posts_test/app/shared/widgets/custom_text_error.dart';
+import 'package:flutter_posts_test/app/view/posts/widgets/custom_post_header.dart';
 import 'package:flutter_posts_test/app/view/posts/widgets/post_card.dart';
 
 class PostsView extends StatefulWidget {
@@ -40,37 +41,41 @@ class _PostsViewState extends State<PostsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Posts', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
-        centerTitle: true,
-        actions: [IconButton(icon: const Icon(Icons.logout), onPressed: logout)],
-      ),
-      body: BlocBuilder<PostBloc, PostState>(
-        bloc: postBloc,
-        builder: (context, state) {
-          if (state is PostLoading || state is PostInitial) {
-            return const Center(child: CustomLoading());
-          }
+      body: CustomScrollView(
+        controller: scrollController,
+        slivers: [
+          SliverFloatingHeader(child: CustomPostHeader()),
+          BlocBuilder<PostBloc, PostState>(
+            bloc: postBloc,
+            builder: (context, state) {
+              if (state is PostLoading || state is PostInitial) {
+                return SliverFillRemaining(child: const Center(child: CustomLoading()));
+              }
 
-          if (state is PostFailure) {
-            return Center(child: CustomTextError(message: state.message));
-          }
+              if (state is PostFailure) {
+                return SliverFillRemaining(
+                  child: Center(child: CustomTextError(message: state.message)),
+                );
+              }
 
-          final posts = (state as PostLoaded).posts;
+              final posts = (state as PostLoaded).posts;
 
-          if (posts.isEmpty) {
-            return const Center(child: Text('Nenhum post encontrado.'));
-          } else {
-            return buildPostsList(state.posts);
-          }
-        },
+              if (posts.isEmpty) {
+                return SliverFillRemaining(
+                  child: const Center(child: Text('Nenhum post encontrado.')),
+                );
+              } else {
+                return buildPostsList(state.posts);
+              }
+            },
+          ),
+        ],
       ),
     );
   }
 
   Widget buildPostsList(List<Post> posts) {
-    return ListView.builder(
-      controller: scrollController,
+    return SliverList.builder(
       itemCount: posts.length + 1,
       itemBuilder: (context, index) {
         if (index == posts.length) return loadMoreButton();
@@ -85,7 +90,7 @@ class _PostsViewState extends State<PostsView> {
 
     if (state.onLoadMore) {
       return const Padding(
-        padding: EdgeInsets.only(top: 8, bottom: 16),
+        padding: EdgeInsets.only(top: 10, bottom: 50),
         child: Center(child: CustomLoading()),
       );
     }
---
+++ flutter_posts_test/lib/app/view/posts/widgets/custom_post_header.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_posts_test/app/bloc/wrapper/wrapper_bloc.dart';
+import 'package:flutter_posts_test/app/bloc/wrapper/wrapper_state.dart';
+import 'package:flutter_posts_test/app/model/user.dart';
+
+class CustomPostHeader extends StatefulWidget {
+  const CustomPostHeader({super.key});
+
+  @override
+  State<CustomPostHeader> createState() => _CustomPostHeaderState();
+}
+
+class _CustomPostHeaderState extends State<CustomPostHeader> {
+  late User user;
+
+  @override
+  void initState() {
+    user = (context.read<WrapperBloc>().state as LoggedIn).user;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        boxShadow: [BoxShadow(color: Color(0x50000000), offset: const Offset(0, 8), blurRadius: 8)],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 14, top: 40, bottom: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [buildImage(), const SizedBox(width: 16), Expanded(child: buildUserInfo())],
+        ),
+      ),
+    );
+  }
+
+  Widget buildImage() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(50),
+      child: Image.network(user.profilePicture, width: 50, height: 50, fit: BoxFit.cover),
+    );
+  }
+
+  Widget buildUserInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            const Text('Bem-vindo, ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+            Text(user.name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ],
+    );
+  }
+}
